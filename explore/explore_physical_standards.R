43c3bb4ba0e1f19a0a704a6fd86fcdd909dc1156
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2023-06-09 09:33:09 +0300

    clean up code

--- explore/explore_physical_standards.R
+++ explore/explore_physical_standards.R
@@ -1,7 +1,7 @@
 source(here::here("R/global.R"))
 
 # General strength (1RM 2 x BW)
-dl <- 155/89
+dl <- 155 / 89
 # Usable upperbody strength (20 reps)
 pu <- 10
 # Speed (13 s)
@@ -18,25 +18,22 @@ phys <- structure(list(type = c("Novice", "PR", "Gang Fit", "Advanced"),
                        speed = c(15, sprint_100m, 13, 12),
                        endurance = c(468, run_1mile, 360, 346),
                        health = c(18, bf, 10, 6)),
-                  row.names = c(NA,-4L),
+                  row.names = c(NA, -4L),
                   class = "data.frame")
 
-# install.packages("devtools")
-# devtools::install_github("ricardo-bion/ggradar")
+install.packages("devtools")
+devtools::install_github("ricardo-bion/ggradar")
 library(ggradar)
 
 normalize <- function(x) {
-  if(any(is.na(x))) stop
-  
-  (x-min(x))/(max(x)-min(x))
+  if (any(is.na(x))) stop("NA values not allowed")
+  (x - min(x)) / (max(x) - min(x))
 }
 
-phys.std <- phys %>%
+phys_std <- phys %>%
   mutate(across(where(~is.numeric(.x)), ~normalize(.x))) %>%
-  mutate(speed = 1-speed,
-         endurance = 1-endurance,
-         health = 1-health)
+  mutate(speed = 1 - speed,
+         endurance = 1 - endurance,
+         health = 1 - health)
 
 ggradar(phys.std)
-
-
