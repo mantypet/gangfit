d2ae1016ae4a8a5cb7aa4ddc1818e8acc4407c61
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2023-06-20 08:41:23 +0300

    minor edit

--- R/global.R
+++ R/global.R
@@ -26,17 +26,19 @@ mark_double_digits <- function(x) {
   x
 }
 
-# read in 20230515_generalstrength.csv
-generalstrength <- read_csv(here::here("data", "20230515_generalstrength.csv")) %>%
-  clean_names() %>%
-  mutate(chinup_reps = mark_double_digits( chinup_reps))
-
-
-# separate squat_reps to multiple columns
-generalstrength_ <- generalstrength %>%
-  separate_wider_regex(cols = squat_reps, pattern = c(squat_reps_1 = "\\d", "", squat_reps_2 = "\\d", "", squat_reps_3 = "\\d")) %>%
-  # copy separate_wider_regex but with press_reps and bench_reps
-  separate_wider_regex(cols = press_reps, pattern = c(press_reps_1 = "\\d", "", press_reps_2 = "\\d", "", press_reps_3 = "\\d")) %>% 
-  separate_wider_regex(cols = bench_reps, pattern = c(bench_reps_1 = "\\d", "", bench_reps_2 = "\\d", "", bench_reps_3 = "\\d")) %>%
-  # again copy separate_wider_regex but with power_clean_reps and to 5 columns
-  separate_wider_regex(cols = power_clean_reps, pattern = c(power_clean_reps_1 = "\\d", "", power_clean_reps_2 = "\\d", "", power_clean_reps_3 = "\\d", "", power_clean_reps_4 = "\\d", "", power_clean_reps_5 = "\\d"))
+todo_function <- function() {
+  # read in 20230515_generalstrength.csv
+  generalstrength <- read_csv(here::here("data", "20230515_generalstrength.csv")) %>%
+    clean_names() %>%
+    mutate(chinup_reps = mark_double_digits( chinup_reps))
+  
+  
+  # separate squat_reps to multiple columns
+  generalstrength_ <- generalstrength %>%
+    separate_wider_regex(cols = squat_reps, pattern = c(squat_reps_1 = "\\d", "", squat_reps_2 = "\\d", "", squat_reps_3 = "\\d")) %>%
+    # copy separate_wider_regex but with press_reps and bench_reps
+    separate_wider_regex(cols = press_reps, pattern = c(press_reps_1 = "\\d", "", press_reps_2 = "\\d", "", press_reps_3 = "\\d")) %>% 
+    separate_wider_regex(cols = bench_reps, pattern = c(bench_reps_1 = "\\d", "", bench_reps_2 = "\\d", "", bench_reps_3 = "\\d")) %>%
+    # again copy separate_wider_regex but with power_clean_reps and to 5 columns
+    separate_wider_regex(cols = power_clean_reps, pattern = c(power_clean_reps_1 = "\\d", "", power_clean_reps_2 = "\\d", "", power_clean_reps_3 = "\\d", "", power_clean_reps_4 = "\\d", "", power_clean_reps_5 = "\\d"))
+}
