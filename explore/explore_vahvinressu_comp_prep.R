b33a38b1118d8fd93d8cc578b32778698cad6acf
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2023-08-07 15:54:47 +0300

    add manual rows

--- explore/explore_vahvinressu_comp_prep.R
+++ explore/explore_vahvinressu_comp_prep.R
@@ -5,6 +5,20 @@ generalstrength <- read_generalstrength(csv_file = here::here("data", "20230803_
 vahvinressu <- generalstrength %>%
   select(date, starts_with("squat"), starts_with("deadlift"), notes)
 
+# Add rows manually, export broken?
+extra <- data.frame(
+  "date" = as.Date(c("2023-08-04", "2023-08-07")),
+  "squat_weight" = c(137.5, 140),
+  "squat_sets_completed" = c(3,3),
+  "squat_reps" = c("555", "555"),
+  "deadlift_weight" = c(NA, 172.5),
+  "deadlift_sets_completed" = c(NA, 1),
+  "deadlift_reps" = c(NA, 5),
+  "notes"   = c("Notes", "Notes")
+)
+vahvinressu <- vahvinressu %>%
+  bind_rows(extra)
+
 time_to_event <- 120
 
 end_date_2022 <- as.Date("2022-08-20")
@@ -31,9 +45,9 @@ vahvinressu23 <- vahvinressu %>%
                                      TRUE ~ deadlift_weight))
 
 ggplot() +
-  geom_point(data = slice_head(vahvinressu22, n = -1), aes(x = t, y = squat_weight), color = "lightgrey") + 
+  geom_point(data = vahvinressu22, aes(x = t, y = squat_weight), color = "lightgrey") + 
   geom_point(data = vahvinressu23, aes(x = t, y = squat_weight), color = "darkgreen") +
-  geom_smooth(data = slice_head(vahvinressu22, n = -1), aes(x = t, y = squat_weight), color = "lightgrey",
+  geom_smooth(data = vahvinressu22, aes(x = t, y = squat_weight), color = "lightgrey",
               method = "loess", fullrange = TRUE, se = FALSE) + 
   geom_smooth(data = vahvinressu23, aes(x = t, y = squat_weight), color = "darkgreen",
               method = "loess", fullrange = TRUE, se = FALSE) +
@@ -50,7 +64,9 @@ ggplot() +
   xlim(-time_to_event, 30) +
   theme_minimal()
 
-mdl <- glm(formula = t_pos ~ deadlift_weight,  family = Gamma(link="log"), data = vahvinressu23)
+mdl <- glm(formula = deadlift_weight ~ t_pos,  family = Gamma(link="log"), data = vahvinressu23)
 
 summary(mdl, dispersion = 1) 
 
+newdata <- data.frame("t_pos" = c(110, 112, 117))
+predict(mdl, newdata, type = "response")
