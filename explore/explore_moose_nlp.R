74231759d3c49e46bdb6a279673c001b885b0e18
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2024-03-15 15:24:13 +0200

    new analysis

--- explore/explore_moose_nlp.R
+++ explore/explore_moose_nlp.R
@@ -7,8 +7,8 @@ vahvinressu <- generalstrength %>%
   filter(date >= "2024-01-01")
 
 vahvinressu.rep <- vahvinressu %>%
-  mutate(week_monday = lubridate::floor_date(date, "week"),
-         week_sunday = lubridate::ceiling_date(date, "week"),
+  mutate(week_monday = lubridate::floor_date(date, "week", week_start = 1),
+         week_sunday = lubridate::ceiling_date(date, "week", week_start = 1),
          squat_weight_lag = lag(squat_weight),
          deadlift_weight_lag = lag(deadlift_weight),
          press_weight_lag = lag(press_weight),
@@ -27,3 +27,54 @@ vahvinressu.week_n <- vahvinressu.rep %>%
             press_weight_start = press_weight_lag[date == min(date)],
             press_weight_increment = sum(press_weight_diff),
             press_weight_increment_rel = (press_weight_start+press_weight_increment)/press_weight_start)
+
+####
+start_value_week <- "2024-03-10"
+start_values <- data.frame(squat_weight_start = vahvinressu.week_n$squat_weight_start[vahvinressu.week_n$week_monday == start_value_week],
+                           deadlift_weight_start = vahvinressu.week_n$deadlift_weight_start[vahvinressu.week_n$week_monday == start_value_week],
+                           press_weight_start = vahvinressu.week_n$press_weight_start[vahvinressu.week_n$week_monday == start_value_week])
+
+str_multip_cycle_1_squat <- 1.02^(0:4)
+str_multip_cycle_1_deadlift <- 1.05^(0:4)
+str_multip_cycle_1_press <- 1.02^(0:4)
+
+str_multip_deload_squat <- max(str_multip_cycle_1_squat)*0.85
+str_multip_deload_deadlift <- max(str_multip_cycle_1_deadlift)*0.85
+str_multip_deload_press <- max(str_multip_cycle_1_press)*0.85
+
+str_multip_cycle_2_squat <- max(str_multip_cycle_1_squat)*1.02^(1:3)
+str_multip_cycle_2_deadlift <- max(str_multip_cycle_1_deadlift)*1.02^(1:3)
+str_multip_cycle_2_press <- max(str_multip_cycle_1_press)*1.02^(1:3)
+
+str_multip_squat <- c(str_multip_cycle_1_squat, str_multip_deload_squat, str_multip_cycle_2_squat)
+str_multip_deadlift <- c(str_multip_cycle_1_deadlift, str_multip_deload_deadlift, str_multip_cycle_2_deadlift)
+str_multip_press <- c(str_multip_cycle_1_press, str_multip_deload_press, str_multip_cycle_2_press)
+
+df <- data.frame(week_index = 1:9,
+                 str_multip) %>%
+  cbind(start_values) %>%
+  mutate(squat_weight = squat_weight_start*str_multip_squat,
+         deadlift_weight = deadlift_weight_start*str_multip_deadlift,
+         press_weight = press_weight_start*str_multip_press)
+
+### Lopulliset syklit
+
+
+cycle <- data.frame(week = seq.Date(from = as.Date("2024-02-05"), to = as.Date("2024-08-12"), by = "week")) %>%
+  mutate(index = 1:length(week),
+         week_num = index+5) %>%
+  mutate(cycle = case_when(index %in% 1:8 ~ 1,
+                           index %in% 9:18 ~ 2,
+                           index %in% 19:28 ~ 3),
+         deload_multip = ifelse(index %in% c(3,8,13,18,23,28), 0.85, 1),
+         multip = (1.05-(log(index)/100))^(index),
+         squat_weight = 105*multip*deload_multip,
+         deadlift_weight = 120*multip*deload_multip,
+         press_weight = 42*multip*deload_multip) %>%
+  mutate(squat_weight_plates = floor(squat_weight / 2.5)*2.5,
+         deadlift_weight_plates = floor(deadlift_weight / 2.5)*2.5,
+         press_weight_plates = floor(press_weight / 1.25)*1.25)
+
+plot(cycle$index, cycle$squat_weight_plates)
+plot(cycle$index, cycle$deadlift_weight_plates)
+plot(cycle$index, cycle$press_weight_plates)
